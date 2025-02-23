ee2b543f55e7c7e041c3e9620e2ce78e2f118660
Andrew Mao <maosteram@gmail.com>
2025-02-23 13:45:49 -0600

    Adds timer

---
+++ background.js
@@ -0,0 +1,70 @@
+let WORK_TIME_SEC = 10;
+let BREAK_TIME_SEC = 15;
+let VIDEO_URL = "https://youtu.be/MKC9LvRivTM?t=7";
+
+chrome.runtime.onInstalled.addListener(async () => {
+  await chrome.storage.local.set({
+    is_on: false,
+    timer: WORK_TIME_SEC,
+    is_break: false,
+    break_tab_id: null,
+  });
+});
+
+chrome.storage.local.get(
+  ["timer", "is_on", "is_break", "break_tab_id"],
+  (data) => {
+    chrome.storage.local.set({
+      timer: "timer" in data ? data.timer : WORK_TIME_SEC,
+      is_on: "is_on" in data ? data.is_on : false,
+      is_break: "is_break" in data ? data.is_break : false,
+      break_tab_id: "break_tab_id" in data ? data.break_tab_id : null,
+    });
+  }
+);
+
+chrome.alarms.create("pomodoroTimer", {
+  periodInMinutes: 1 / 60,
+});
+
+chrome.alarms.onAlarm.addListener(async (alarm) => {
+  if (alarm.name === "pomodoroTimer") {
+    chrome.storage.local.get(["is_on", "timer", "is_break"], (data) => {
+      if (data.is_on && data.timer > 0) {
+        chrome.storage.local.set({ timer: data.timer - 1 });
+        console.log(`Timer: ${data.timer}`);
+      } else if (data.is_on && data.timer === 0) {
+        if (!data.is_break) {
+          startBreak();
+        } else {
+          restartWorkSession();
+        }
+      }
+    });
+  }
+});
+
+const startBreak = () => {
+  console.log("Starting break time:");
+  chrome.tabs.create({ url: VIDEO_URL, active: true }, (tab) => {
+    chrome.storage.local.set({
+      is_break: true,
+      timer: BREAK_TIME_SEC,
+      break_tab_id: tab.id,
+    });
+
+    setTimeout(() => {
+      chrome.tabs.remove(tab.id, () => console.log("Closed break video"));
+      chrome.storage.local.set({ break_tab_id: null });
+    }, BREAK_TIME_SEC * 1000);
+  });
+};
+
+const restartWorkSession = () => {
+  console.log("Restarting work session");
+
+  chrome.storage.local.set({
+    is_break: false,
+    timer: WORK_TIME_SEC,
+  });
+};
---
+++ script.js
@@ -0,0 +1,67 @@
+const DEFAULT_WORK_TIME = 10;
+let timerIntervalId;
+
+const timerDisplay = document.getElementById("timer");
+const startButton = document.getElementById("start");
+const resetButton = document.getElementById("reset");
+
+const setTimerDisplay = (time) => {
+  const minutes = Math.floor(time / 60);
+  const seconds = time % 60;
+  timerDisplay.textContent = `${minutes}:${seconds
+    .toString()
+    .padStart(2, "0")}`;
+};
+
+const updateTimer = () => {
+  chrome.storage.local.get(["timer", "is_break"], (data) => {
+    setTimerDisplay(data.timer);
+    if (data.is_break) {
+      startButton.style.display = "none";
+      resetButton.style.display = "none";
+      document.getElementById("break-message").style.display = "block";
+    } else {
+      startButton.style.display = "inline-block";
+      resetButton.style.display = "inline-block";
+      document.getElementById("break-message").style.display = "none";
+    }
+  });
+};
+
+const startTimer = () => {
+  timerIntervalId = setInterval(updateTimer, 1000);
+  startButton.textContent = "Stop";
+};
+
+updateTimer();
+chrome.storage.local.get(["is_on"], (data) => {
+  if (data.is_on) {
+    startTimer();
+  }
+});
+
+const stopTimer = () => {
+  clearInterval(timerIntervalId);
+  startButton.textContent = "Start";
+};
+
+startButton.addEventListener("click", () => {
+  chrome.storage.local.get(["is_on"], (data) => {
+    if (data.is_on) {
+      stopTimer();
+      chrome.storage.local.set({ is_on: false });
+    } else {
+      startTimer();
+      chrome.storage.local.set({ is_on: true });
+    }
+  });
+});
+
+resetButton.addEventListener("click", async () => {
+  stopTimer();
+  setTimerDisplay(DEFAULT_WORK_TIME);
+  await chrome.storage.local.set({
+    timer: DEFAULT_WORK_TIME,
+    is_on: false,
+  });
+});
